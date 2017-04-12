59fc169644dcc70820b2675e89688d880a43c3d4
gfdgeras <gfdgeras@gmail.com>
2017-04-11 18:49:15 -0700

    Coordenadas lineas

--- Programa Principal/linea.py
+++ Programa Principal/linea.py
@@ -20,7 +20,9 @@ while(True):
     lines = cv2.HoughLinesP(mask,1,np.pi/180,100,minLineLength,maxLineGap)
     try:
         for x1,y1,x2,y2 in lines[1]:
-            cv2.line(frame,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.circle(frame,(x1,y1),5,(0,0,255),-1)
+            cv2.circle(frame,(x2,y2),5,(255,0,0),-1)
+            print x1
     except:
         continue
     
